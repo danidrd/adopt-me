7a18fa7978265e44d04708b8715992bc494ed987
danidrd <danielecaliandro@gmail.com>
2023-09-27 14:09:43 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Details from "./Details";
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />}/>
+        </Routes>
+      </Router>
     </div>
   );
 };
 const root = createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
