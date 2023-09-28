534f6709d31cfefa326c58478fdef0fd0dc14655
danidrd <danielecaliandro@gmail.com>
2023-09-28 16:34:32 +0200

    fifth commit

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import withRouter from "./withRouter";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   constructor() {
@@ -9,23 +10,23 @@ class Details extends Component {
 
   async componentDidMount() {
     const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?id=${this.props.router.params.id}`
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.router.params.id}`,
     );
     const json = await res.json();
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
 
   render() {
-    console.log(this.state);
-
     if (this.state.loading) {
       return <h2>loading … </h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
